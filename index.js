ec00dda9a1ea315fd490daf85172894b90f69015
kljnkhki <isjcllywilliams@hotmail.com>
2024-05-08 22:05:36 +0700

    update task

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-function isValidBST(root) {
-  return isValid(root, null, null);
-  function isValid(node, min, max) {
-    if (!node) return true;
-    if ((min !== null && node.val <= min) || (max !== null && node.val >= max))
-      return false;
-    return (
-      isValid(node.left, min, node.val) && isValid(node.right, node.val, max)
-    );
-  }
+function isPalindrome(str) {
+  return str === str.split("").reverse().join("");
 }
